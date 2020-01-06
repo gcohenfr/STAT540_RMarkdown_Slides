2a612d974aef4b01c4f558c3816323557de842fc
kdkorthauer <kdkorthauer@gmail.com>
2020-01-06 10:48:38 -0800

    clean up L5 fig files; add script to generate pdfs

---
+++ generate_pdfs.R
@@ -0,0 +1,5 @@
+# this script is only needed to generate pdf slide output
+# requires that html output is present
+pagedown::chrome_print("L5_TwoGroupComp/L5_TwoGroupComp.html")
+pagedown::chrome_print("L6_ANOVA/L6_ANOVA.html")
+pagedown::chrome_print("L7_LinearModels/L7_LinearModels.html")
