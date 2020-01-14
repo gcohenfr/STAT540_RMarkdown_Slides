08098d241327f8212b14632a9e08c842fcef2d24
kdkorthauer <kdkorthauer@gmail.com>
2020-01-14 15:53:23 -0800

    minor edits L4; generate L4 pdf

--- generate_pdfs.R
+++ generate_pdfs.R
@@ -1,5 +1,6 @@
 # this script is only needed to generate pdf slide output
 # requires that html output is present
+pagedown::chrome_print("L4_StatReview/L4_StatReview.html")
 pagedown::chrome_print("L5_TwoGroupComp/L5_TwoGroupComp.html")
 pagedown::chrome_print("L6_ANOVA/L6_ANOVA.html")
 pagedown::chrome_print("L7_LinearModels/L7_LinearModels.html")
