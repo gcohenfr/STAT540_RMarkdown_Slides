46705eaaac0a3b0617ea9938f019095c072154b2
kdkorthauer <kdkorthauer@gmail.com>
2020-01-18 17:57:15 -0800

    increase timeout lim for pdf generation

--- generate_pdfs.R
+++ generate_pdfs.R
@@ -1,6 +1,6 @@
 # this script is only needed to generate pdf slide output
 # requires that html output is present
-pagedown::chrome_print("lect04_StatReview/lect04_StatReview.html", timeout=300)
-pagedown::chrome_print("lect05_TwoGroupComp/lect05_TwoGroupComp.html", timeout=300)
-pagedown::chrome_print("lect06_ANOVA/lect06_ANOVA.html", timeout=300)
-pagedown::chrome_print("lect07_LinearModels/lect07_LinearModels.html", timeout=300)
+pagedown::chrome_print("lect04_StatReview/lect04_StatReview.html", timeout=1000)
+pagedown::chrome_print("lect05_TwoGroupComp/lect05_TwoGroupComp.html", timeout=1000)
+pagedown::chrome_print("lect06_ANOVA/lect06_ANOVA.html", timeout=1000)
+pagedown::chrome_print("lect07_LinearModels/lect07_LinearModels.html", timeout=1000)
