a5d15efb68669208ba876bbf5953ce68e8aff3eb
kdkorthauer <kdkorthauer@gmail.com>
2020-01-28 22:18:23 -0800

    udpate L8

--- generate_pdfs.R
+++ generate_pdfs.R
@@ -4,3 +4,5 @@ pagedown::chrome_print("lect04_StatReview/lect04_StatReview.html", timeout=1000)
 pagedown::chrome_print("lect05_TwoGroupComp/lect05_TwoGroupComp.html", timeout=1000)
 pagedown::chrome_print("lect06_ANOVA/lect06_ANOVA.html", timeout=1000)
 pagedown::chrome_print("lect07_LinearModels/lect07_LinearModels.html", timeout=1000)
+pagedown::chrome_print("lect08_Continuous/lect08_Continuous.html", timeout=1000)
+
